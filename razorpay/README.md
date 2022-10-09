4568416e601630fdbbbe5daa6b6513656cc4016d
vjspranav <pranavasri@live.in>
2022-10-09 13:25:23 +0530

    Add a simple razor pay backend * will work independently

---
+++ razorpay/README.md
@@ -0,0 +1,19 @@
+# Razorpay microservice gateway
+
+## Description
+
+This application aims to be a centralised payment gateway through which any merchant can credate orders and accept payments.
+
+## Instructions
+
+- Create a python virtual environment
+  On linux, do:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+- Run `pip install -r requirements.txt`
+
+- python3 server.py
---
+++ razorpay/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import razorpay
+
+app = Flask(__name__)
+
+# Create an application that creates razorpay order and other things using rest api requests
+
+@app.route('/create_order', methods=['POST'])
+def create_order():
+    # Get key_id and key_secret from request header
+    key_id = request.headers.get('rp_key_id')
+    key_secret = request.headers.get('rp_key_secret')
+    # Get amount from request body
+    amount = request.json.get('amount')
+
+    # Create razorpay client
+    client = razorpay.Client(auth=(key_id, key_secret))
+
+    # reciept from request body
+    receipt = request.json.get('receipt')
+
+    # Create order
+    order = client.order.create({'amount': amount, 'currency': 'INR', 'receipt': receipt})
+
+    # Return order id
+    return order
+ 
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'Hello World'

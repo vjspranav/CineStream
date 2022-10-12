eae125861f63ccee1ae86dbc1ecd98ed15dffc1f
vjspranav <pranavasri@live.in>
2022-10-12 19:48:18 +0530

    Render all movies from server

--- cinestream-backend/routes/index.js
+++ cinestream-backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const auth = require("../middleware/auth");
+const axios = require("axios");
+
 const router = express.Router();
+const urls = require("../keys/servers");
 
 /* GET home page. */
 router.get("/", auth, function (req, res, next) {
@@ -13,6 +16,43 @@ router.get("/", auth, function (req, res, next) {
  */
 router.get("/service-status", (req, res) => {
   res.status(200).json({ status: "online" });
+});
+
+// Send all movies of all platforms
+router.get("/all-content", async (req, res) => {
+  data = {};
+  try {
+    allOTT = urls.OTT_SERVERS;
+    for (let i = 0; i < allOTT.length; i++) {
+      const response = await axios.get(allOTT[i].url + "/");
+      data[allOTT[i].name] = response.data;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
+  res.status(200).json(data);
+});
+
+// Send a particular movie
+router.get("/movie/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  try {
+    allOTT = urls.OTT_SERVERS;
+    for (let i = 0; i < allOTT.length; i++) {
+      const response = await axios.get(allOTT[i].url + "/");
+      let data = response.data;
+      if (movieId in data) {
+        return res.status(200).json(data[movieId]);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+
+  return res.status(400).json({ error: "Movie not found" });
 });
 
 module.exports = router;
--- cinestream-frontend/src/pages/Home.js
+++ cinestream-frontend/src/pages/Home.js
@@ -1,23 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { useNavigate } from "react-router-dom";
+import { SpinnerInfinity } from "spinners-react";
 import ContentScroller from "../components/ContentScroller";
-import content from "./content.json";
+import Constants from "../Constants";
+import axios from "axios";
 
 const Home = () => {
   // Set interval run every 5 seconds
   const [index, setIndex] = useState(0);
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
-  return (
+  const navigate = useNavigate();
+  const [data, setData] = useState({});
+  const [content, setContent] = useState([]);
+  useEffect(() => {
+    const init = async () => {
+      await axios.get(Constants.BACKEND_URL + "/all-content").then((res) => {
+        console.log(res.data);
+        let data = res.data;
+        setData(data);
+        let tempContent = [];
+        for (let i in data) {
+          let tempAllContent = Object.values(data[i]);
+          tempContent = [...tempContent, ...tempAllContent];
+        }
+        console.log(tempContent);
+        setContent(tempContent);
+        setLoading(false);
+      });
+    };
+    init();
+  }, []);
+
+  const Loader = (
+    <SpinnerInfinity
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+      size={100}
+      thickness={120}
+      speed={83}
+      color="rgba(57, 172, 163, 1)"
+      secondaryColor="rgba(57, 115, 172, 0.44)"
+    />
+  );
+
+  const Home = (
     <div className="home">
       <div className="home-carousel">
         <Carousel fade activeIndex={index} onSelect={setIndex}>
-          {Object.values(content).map((item) => (
+          {content.map((item) => (
             <Carousel.Item
               key={item.id}
-              style={{cursor: "pointer"}}
-              onClick={() => navigate(`/movieViewer`, {state: { movieID: item.id }})}
+              style={{ cursor: "pointer" }}
+              onClick={() =>
+                navigate(`/movieViewer/${item.id}`, { replace: true })
+              }
             >
               <img
                 img-responsive={1}
@@ -37,16 +79,23 @@ const Home = () => {
           ))}
         </Carousel>
       </div>
-      <div className="home-content">
-        <h2 style={{ marginLeft: "10px" }}>OTT-A</h2>
-        <ContentScroller data={content} />
-      </div>
-      <div className="home-content">
-        <h2>OTT-A</h2>
-        <ContentScroller data={Object.values(content).slice(0, 5)} />
-      </div>
+      {Object.keys(data).map((key) => {
+        return (
+          <div key={key}>
+            <h2
+              style={{
+                marginLeft: "10px",
+              }}
+            >
+              {key}
+            </h2>
+            <ContentScroller data={data[key]} />
+          </div>
+        );
+      })}
     </div>
   );
+  return loading ? Loader : Home;
 };
 
 export default Home;
